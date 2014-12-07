2985f8a51924460da8fff697601ba1cf909c39c4
Wade Sellers <Waderoni@gmail.com>
2014-12-07 05:32:41 -0700

    Initial Commit

---
+++ MyPortfolio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPortfolio
+//
+//  Created by Wade Sellers on 12/7/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyPortfolio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPortfolio
+//
+//  Created by Wade Sellers on 12/7/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
